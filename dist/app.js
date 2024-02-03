0c19ac73444519878358ed3afa1bed1348699f6b
Heeyoung Yang <dev.heeyoung@gmail.com>
2024-02-03 14:12:22 -0500

    add PageItemComponent

--- dist/app.js
+++ dist/app.js
@@ -7,11 +7,11 @@ class App {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
         const image = new ImageComponent('Testing Image', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend');
+        this.page.addChild(image);
         const note = new NoteComponent('title', 'body');
-        note.attachTo(appRoot, 'beforeend');
+        this.page.addChild(note);
         const video = new VideoComponent('video title', 'https://www.youtube.com/watch?v=JP3R00tCL4I&ab_channel=StudySonicFocus');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
     }
 }
 new App(document.querySelector('.document'));
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
-import { PageComponent } from './components/page/page.js'
+import { Composable, PageComponent } from './components/page/page.js'
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { VideoComponent } from './components/page/item/video.js';
+import { Component } from './components/component.js';
 
 
 class App {
-    private readonly page: PageComponent;
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent('Testing Image', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend')
+        this.page.addChild(image);
 
         const note = new NoteComponent('title', 'body');
-        note.attachTo(appRoot, 'beforeend')
+        this.page.addChild(note);
 
         const video = new VideoComponent('video title', 'https://www.youtube.com/watch?v=JP3R00tCL4I&ab_channel=StudySonicFocus')
-        video.attachTo(appRoot, 'beforeend')
+        this.page.addChild(video);
     }
 }
 
