58af6d107a28fae48a668e171a98fff1e240790b
Heeyoung Yang <dev.heeyoung@gmail.com>
2024-02-03 10:46:17 -0500

    add noteComponent

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,14 @@
 import { PageComponent } from './components/page/page.js';
 import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
         const image = new ImageComponent('Testing Image', 'https://picsum.photos/600/300');
         image.attachTo(appRoot, 'beforeend');
+        const note = new NoteComponent('title', 'body');
+        note.attachTo(appRoot, 'beforeend');
     }
 }
 new App(document.querySelector('.document'));
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { PageComponent } from './components/page/page.js'
 import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
 
 
 class App {
@@ -10,6 +11,9 @@ class App {
 
         const image = new ImageComponent('Testing Image', 'https://picsum.photos/600/300');
         image.attachTo(appRoot, 'beforeend')
+
+        const note = new NoteComponent('title', 'body');
+        note.attachTo(appRoot, 'beforeend')
     }
 }
 
