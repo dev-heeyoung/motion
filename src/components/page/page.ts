94ede5fe767058d2e2f5fec588417f1b262b246e
Heeyoung Yang <dev.heeyoung@gmail.com>
2024-02-03 14:31:33 -0500

    add delete item feature

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,24 +4,32 @@ export interface Composable {
     addChild(child: Component): void;
 }
 
+type OnCloseListener = () => void;
+
 export class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+    private closeListener?: OnCloseListener;
     constructor() {
         super(`
         <li class="page-item">
             <section class="page-item__body"></section>
             <div class="page-item__controls">
-                <span class="close">X</span>
+                <button class="close">X</button>
             </div>
         </li>
-    `)
+        `)
+        const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+        closeBtn.onclick = () => this.closeListener && this.closeListener();
     }
     addChild(child: Component) {
         const container = this.element.querySelector('.page-item__body')! as HTMLElement;
         child.attachTo(container);
     }
+    setOnCloseListener(listener: OnCloseListener) {
+        this.closeListener = listener;
+    }
 }
 
-export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
+export class PageComponent extends BaseComponent<HTMLUListElement> {
     constructor() {
         super(`<ul class='page'></ul>`)
     } 
@@ -29,5 +37,8 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
        const item = new PageItemComponent();
        item.addChild(section);
        item.attachTo(this.element, 'beforeend')
+       item.setOnCloseListener(() => {
+        item.removeFrom(this.element);
+       })
     }
 }
